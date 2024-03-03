7b92ff0983ed9f5a5b6f55d8203e60e8f54a0ac4
Naman Kharbanda <namankharbanda512@gmail.com>
2024-03-03 18:02:37 +0530

    Further Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Navigate, Route, Routes } from "react-router-dom";
 import About from "./pages/about/About";
+import Date from "./pages/about/Date";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Navigate to="/about" />} />
         <Route path="/about" element={<About />} />
+        {/* <Route path="/date" element={<Date />} /> */}
       </Routes>
     </div>
   );
